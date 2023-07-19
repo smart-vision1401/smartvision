039fa9f7f48553a16830e262ef2a38d85f65ff33
smart-vision1401 <138372679+smart-vision1401@users.noreply.github.com>
2023-07-19 04:00:00 -0700

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x22613819ba5c977b0a8a3e56f5ce17f1fad447f7";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
